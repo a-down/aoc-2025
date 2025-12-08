40067abef15f032cc99c503ef8faa46f82640e63
a-down <alec@characterstrong.com>
2025-12-08 08:13:02 -0600

    day 2 part 1

--- day-2/gift-shop.swift
+++ day-2/gift-shop.swift
@@ -1,34 +1,73 @@
 import Foundation
 
-func getRangesFromFile(isTest: Bool) -> [(start: Int, end: Int)] {
-  let currentDirectoryPath = FileManager().currentDirectoryPath
-  let fileName: String = isTest ? "test-input.txt" : "input.txt"
+let arguments = CommandLine.arguments.dropFirst()  // Drop the executable path
 
-  do {
-    let baseUrl = URL(fileURLWithPath: currentDirectoryPath)
-    let fileURL = baseUrl.appendingPathComponent(fileName)
-
-    let fileContents: String = try String(contentsOf: fileURL, encoding: .utf8)
-    let fileArray = fileContents.split(separator: ",")
-    return fileArray.map { Substring in
-      let bookends = Substring.split(separator: "-")
-      let startInt = Int(bookends[0])
-      let endInt = Int(bookends[1])
-
-      guard let unwrappedStartInt = startInt, let unwrappedEndInt = endInt else {
-        return (start: 0, end: 0)
-      }
-      return (start: unwrappedStartInt, end: unwrappedEndInt)
-
-      // return (start: startInt, end: endInt)
-
-      // return String(Substring)
-    }
-
-  } catch {
-    print("Error reading from file: \(error)")
-    return []
+var isTest = false
+if let firstArg = arguments.first {
+  if firstArg == "true" {
+    print("\nIS TEST\n")
+    isTest = true
   }
 }
 
-print(getRangesFromFile(isTest: true))
+func getFileText(fileName: String) -> String {
+  let currentDirectoryPath = FileManager().currentDirectoryPath
+
+  let baseUrl = URL(fileURLWithPath: currentDirectoryPath)
+  let fileURL = baseUrl.appendingPathComponent(fileName)
+
+  do {
+    let fileContents: String = try String(contentsOf: fileURL, encoding: .utf8)
+    return fileContents
+  } catch {
+    print("Error reading from file: \(error)")
+    return ""
+  }
+}
+
+func getRangesFromFile() -> [(Int, Int)] {
+  let fileContents = getFileText(fileName: isTest ? "test-input.txt" : "input.txt")
+  let fileArray = fileContents.split(separator: ",")
+  return fileArray.map { Substring in
+    let bookends = Substring.split(separator: "-")
+    let startInt = Int(bookends[0])
+    let endInt = Int(bookends[1])
+
+    guard let unwrappedStartInt = startInt, let unwrappedEndInt = endInt else {
+      return (start: 0, end: 0)
+    }
+    return (unwrappedStartInt, unwrappedEndInt)
+  }
+}
+
+func getInvalidIdsValueFromRange(range: (Int, Int)) -> Int {
+  let allIdsInRange = Array(range.0...range.1)
+  var valueOfInvalidIds = 0
+
+  print(range)
+
+  for id in allIdsInRange {
+    let splitId = String(id).split(separator: "")
+    let halfLength = splitId.count / 2
+    let firstHalf = splitId[0..<halfLength].joined(separator: "")
+    let secondHalf = splitId[halfLength..<splitId.count].joined(separator: "")
+
+    if firstHalf == secondHalf {
+      valueOfInvalidIds = valueOfInvalidIds + id
+    }
+  }
+
+  return valueOfInvalidIds
+}
+
+func getGiftShopInvalidIdSum() {
+  var sum = 0
+  let ranges = getRangesFromFile()
+  for range in ranges {
+    let rangeValue = getInvalidIdsValueFromRange(range: range)
+    sum = sum + rangeValue
+  }
+  print(sum)
+}
+
+getGiftShopInvalidIdSum()

260beec37a548152ae85f059a33d5fa62d965c5a
a-down <alec@characterstrong.com>
2025-12-13 20:26:31 -0600

    progress on day-3 part 2

--- day-3/lobby.swift
+++ day-3/lobby.swift
@@ -32,12 +32,47 @@ func getBatteryBanks() -> [String] {
   }
 }
 
-func getBatteryBankJoltage(batteryBank: String) -> Int {
+func getBatteryBankJoltage(batteryBank: String, batteryLimit: Int) -> Int {
   let splitBatteryBank: [String] = batteryBank.split(separator: "").map { Substring in
     String(Substring)
   }
 
   var highestJoltage = 0
+
+  var splitBatteryBankWithRemoved = splitBatteryBank
+  splitBatteryBankWithRemoved.removeLast(batteryLimit - 1)
+
+  outerLoop: for i in (1...9).reversed() {
+    if let iIdx = splitBatteryBankWithRemoved.firstIndex(of: String(i)) {
+      print("iIdx", iIdx, i)
+      var splitBatterBankWithoutStart = splitBatteryBank
+      splitBatterBankWithoutStart.removeSubrange(0...iIdx)
+      print(splitBatteryBank)
+      print("w/o start \(splitBatterBankWithoutStart)")
+
+      for j: Int in (1...9).reversed() {
+        print(i, j)
+        if let jIdx = splitBatteryBank.indices(of: String(j))
+        {
+          print("indexes", iIdx, jIdx)
+          if iIdx < jIdx {
+            let joltage = Int("\(i)\(j)")
+            guard let joltage = joltage else {
+              continue
+            }
+            print("joltage", joltage, highestJoltage)
+            if joltage > highestJoltage {
+              highestJoltage = joltage
+            }
+            break outerLoop
+          }
+        }
+
+      }
+    }
+  }
+
+  var actualHighestJoltage = 0
   outerLoop: for idx in 0...(splitBatteryBank.count - 2) {
     for jdx in 0...(splitBatteryBank.count - 1) {
       if idx >= jdx {
@@ -47,14 +82,19 @@ func getBatteryBankJoltage(batteryBank: String) -> Int {
       guard let unwrappedJoltage = joltage else {
         continue
       }
-      if unwrappedJoltage > highestJoltage {
-        highestJoltage = unwrappedJoltage
+      if unwrappedJoltage > actualHighestJoltage {
+        actualHighestJoltage = unwrappedJoltage
       }
     }
   }
 
-  print(("Battery Bank: \(batteryBank)"))
-  print("Joltage: \(highestJoltage)")
+  // print(("Battery Bank: \(batteryBank)"))
+  // print("Joltage: \(highestJoltage)")
+  if highestJoltage != actualHighestJoltage {
+    print("Battery Bank: \(batteryBank)")
+    print("Incorrect Highest Joltage: \(highestJoltage)")
+    print("Actual Highest Joltage: \(actualHighestJoltage)")
+  }
 
   return highestJoltage
 }
@@ -63,8 +103,15 @@ func printTotalJoltage() {
   var totalJoltage = 0
 
   for batteryBank in getBatteryBanks() {
-    totalJoltage += getBatteryBankJoltage(batteryBank: batteryBank)
+    totalJoltage += getBatteryBankJoltage(batteryBank: batteryBank, batteryLimit: 2)
   }
 
   print("Total Joltage: \(totalJoltage)")
 }
+
+// printTotalJoltage()
+print(
+  getBatteryBankJoltage(
+    batteryBank:
+      "7553154621162252232354352224245561873423127442553452398445224242243443525234625124162152237227454463",
+    batteryLimit: 2))
